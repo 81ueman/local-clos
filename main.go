d551020a4903af5c113ae119c3b6b43a191965af
Daichi Suzuki <mathmathds@gmail.com>
2023-11-11 15:31:37 +0900

    est arp_accept=1 to update the arp table

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Description: local-closのメインプログラム
 package main
 
 import (
@@ -12,6 +13,15 @@ import (
 	"github.com/mdlayher/arp"
 )
 
+/*
+ifiがループバックインターフェースかどうかを判定する
+*/
+func is_loopback(ifi net.Interface) bool {
+	return ifi.Flags&net.FlagLoopback != 0
+}
+
+// ifiからIPv4アドレスを取得する
+// arpパッケージで使うことを見越して返り値をnetip.Addrにしている
 func get_ipv4(ifi net.Interface) (netip.Addr, error) {
 	addrs, err := ifi.Addrs()
 	if err != nil {
@@ -35,20 +45,51 @@ func get_ipv4(ifi net.Interface) (netip.Addr, error) {
 	return ip, nil
 }
 
+// ifiからGARPを送信する
+// 対向機器に自分自身のIPアドレスを通知するために使用しているだけなので
+// GARPである必要はないがまあ便利なので
 func send_garp(ifi net.Interface) {
 	ip, err := get_ipv4(ifi)
 	if err != nil {
 		log.Fatalf("failed to get ipv4 addr: %v", err)
 	}
-	packet, err := arp.NewPacket(arp.OperationReply, ifi.HardwareAddr, ip, ifi.HardwareAddr, ip)
+	log.Printf("local address is %v", ip)
+	log.Printf("hardware address is %v", ifi.HardwareAddr)
+	broad, err := net.ParseMAC("ff:ff:ff:ff:ff:ff")
+	if err != nil {
+		log.Fatalf("failed to parse mac: %v", err)
+	}
+	packet, err := arp.NewPacket(arp.OperationReply, ifi.HardwareAddr, ip, broad, ip)
 	if err != nil {
 		log.Fatalf("failed to create arp packet: %v", err)
 	}
+	log.Printf("packet is %v", packet)
 	cli, err := arp.Dial(&ifi)
 	if err != nil {
 		log.Fatalf("failed to dial: %v", err)
 	}
-	cli.WriteTo(packet, ifi.HardwareAddr)
+	err = cli.WriteTo(packet, broad)
+	if err != nil {
+		log.Fatalf("failed to write: %v", err)
+	}
+}
+
+// send garp packet from all interfaces to maintain opposite arp table
+func maintain_arptable() {
+	for {
+		ifis, err := net.Interfaces()
+		if err != nil {
+			log.Fatalf("failed to get interfaces: %v", err)
+		}
+		for _, ifi := range ifis {
+			if is_loopback(ifi) {
+				continue
+			}
+			log.Printf("sending garp from %v", ifi.Name)
+			send_garp(ifi)
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
 
 func main() {
@@ -56,21 +97,7 @@ func main() {
 		log.Fatal("usage: ./local-clos [active|passive]")
 	}
 	mode := os.Args[1]
-	go func() {
-		for {
-			ifis, err := net.Interfaces()
-			if err != nil {
-				log.Fatalf("failed to get interfaces: %v", err)
-			}
-			for _, ifi := range ifis {
-				if ifi.Flags&net.FlagLoopback != 0 {
-					continue
-				}
-				send_garp(ifi)
-			}
-			time.Sleep(10 * time.Second)
-		}
-	}()
+	go maintain_arptable()
 
 	if mode == "active" {
 		active_mode()
--- make_netns.sh
+++ make_netns.sh
@@ -1,25 +1,66 @@
 #!/bin/sh
 
 # Remove the network namespace if it already exists
-ip netns del ns1
-ip netns del ns2
+ip netns del spine-1
+ip netns del spine-2
+ip netns del leaf-1
+ip netns del leaf-2
 # Create a network namespace
-ip netns add ns1
-ip netns add ns2
+ip netns add spine-1
+ip netns add spine-2
+ip netns add leaf-1
+ip netns add leaf-2
 # Create a veth pair
-ip link add veth1 type veth peer name veth2
+ip link add veth-s1-l1-s type veth peer name veth-s1-l1-l
+ip link add veth-s1-l2-s type veth peer name veth-s1-l2-l
+ip link add veth-s2-l1-s type veth peer name veth-s2-l1-l
+ip link add veth-s2-l2-s type veth peer name veth-s2-l2-l
 # Add the peer interfaces to the namespaces
-ip link set veth1 netns ns1
-ip link set veth2 netns ns2
+# いい感じにこの後の設定書いて
+ip link set veth-s1-l1-s netns spine-1
+ip link set veth-s1-l1-l netns leaf-1
+ip link set veth-s1-l2-s netns spine-1
+ip link set veth-s1-l2-l netns leaf-2
+ip link set veth-s2-l1-s netns spine-2
+ip link set veth-s2-l1-l netns leaf-1
+ip link set veth-s2-l2-s netns spine-2
+ip link set veth-s2-l2-l netns leaf-2
 # Assign IP addresses
-ip netns exec ns1 ip addr add 192.168.0.1/24 dev veth1
-ip netns exec ns2 ip addr add 192.168.0.2/24 dev veth2
+# いい感じにこの後の設定書いて
+ip netns exec spine-1 ip addr add 10.1.1.1/24 dev veth-s1-l1-s
+ip netns exec leaf-1 ip addr add 10.1.1.2/24 dev veth-s1-l1-l
+ip netns exec spine-1 ip addr add 10.1.2.1/24 dev veth-s1-l2-s
+ip netns exec leaf-2 ip addr add 10.1.2.2/24 dev veth-s1-l2-l
+ip netns exec spine-2 ip addr add 10.2.1.1/24 dev veth-s2-l1-s
+ip netns exec leaf-1 ip addr add 10.2.1.2/24 dev veth-s2-l1-l
+ip netns exec spine-2 ip addr add 10.2.2.1/24 dev veth-s2-l2-s
+ip netns exec leaf-2 ip addr add 10.2.2.2/24 dev veth-s2-l2-l
 # Bring up the interfaces
-ip netns exec ns1 ip link set dev lo up
-ip netns exec ns1 ip link set dev veth1 up
-ip netns exec ns2 ip link set dev lo up
-ip netns exec ns2 ip link set dev veth2 up
+ip netns exec spine-1 ip link set dev veth-s1-l1-s up
+ip netns exec leaf-1 ip link set dev veth-s1-l1-l up
+ip netns exec spine-1 ip link set dev veth-s1-l2-s up
+ip netns exec leaf-2 ip link set dev veth-s1-l2-l up
+ip netns exec spine-2 ip link set dev veth-s2-l1-s up
+ip netns exec leaf-1 ip link set dev veth-s2-l1-l up
+ip netns exec spine-2 ip link set dev veth-s2-l2-s up
+ip netns exec leaf-2 ip link set dev veth-s2-l2-l up
 
-# Alias for command in each namespace
-alias ns1="sudo ip netns exec ns1"
-alias ns2="sudo ip netns exec ns2"
+ip netns exec spine-1 ip link set dev lo up
+ip netns exec spine-2 ip link set dev lo up
+ip netns exec leaf-1 ip link set dev lo up
+ip netns exec leaf-2 ip link set dev lo up
+
+
+ip netns exec spine-1 sysctl -w net.ipv4.conf.veth-s1-l1-s.arp_accept=1
+ip netns exec spine-1 sysctl -w net.ipv4.conf.veth-s1-l2-s.arp_accept=1
+ip netns exec spine-2 sysctl -w net.ipv4.conf.veth-s2-l1-s.arp_accept=1
+ip netns exec spine-2 sysctl -w net.ipv4.conf.veth-s2-l2-s.arp_accept=1
+ip netns exec leaf-1 sysctl -w net.ipv4.conf.veth-s1-l1-l.arp_accept=1  
+ip netns exec leaf-1 sysctl -w net.ipv4.conf.veth-s2-l1-l.arp_accept=1
+ip netns exec leaf-2 sysctl -w net.ipv4.conf.veth-s1-l2-l.arp_accept=1
+ip netns exec leaf-2 sysctl -w net.ipv4.conf.veth-s2-l2-l.arp_accept=1
+
+echo 'alias s1="sudo ip netns exec spine-1"' > tmp
+echo 'alias s2="sudo ip netns exec spine-2"' >> tmp
+echo 'alias l1="sudo ip netns exec leaf-1"' >> tmp
+echo 'alias l2="sudo ip netns exec leaf-2"' >> tmp
