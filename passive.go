a25993b6ddf0aa92985d8d91a455f6c92347b54e
Daichi Suzuki <mathmathds@gmail.com>
2023-11-14 22:37:26 +0900

    refactor passive.go as FSM

--- passive.go
+++ passive.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"log"
 	"net"
+	"time"
 
 	"github.com/81ueman/local-clos/header"
 	"github.com/81ueman/local-clos/open"
@@ -14,29 +15,80 @@ type open_MSG struct {
 	open.Open
 }
 
-func handle_connection(conn net.Conn) {
-	defer conn.Close()
+func handle_bgp_passive(ifi net.Interface) {
+	session := Session{
+		State:               Idle,
+		ConnectRetryCounter: 0,
+		ConnectRetryTime:    120 * time.Second,
+		HoldTime:            180 * time.Second,
+		KeepaliveTime:       60 * time.Second,
+	}
+
+	events := make(chan Event)
+
 	for {
-		msg := open_MSG{}
-		err := binary.Read(conn, binary.BigEndian, &msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			return
+		log.Print("session state: ", session.State)
+		switch session.State {
+		case Idle:
+			go func(session *Session) {
+				conn, err := wait_tcp(ifi)
+				if err != nil {
+					log.Fatalf("failed to wait tcp: %v", err)
+					events <- TcpConnectionFails
+				}
+				session.Conn = conn
+				events <- Tcp_CR_Acked
+			}(&session)
+			session.State = Active
+		case Connect:
+			// Should't reach here
+			log.Fatal("shouldn't reach here")
+		case Active:
+			event := <-events
+			switch event {
+			case Tcp_CR_Acked:
+				open_msg := open.New(4, 65000, 180, 0)
+				bytes, err := Marshal(open_msg)
+				if err != nil {
+					log.Fatalf("failed to marshal: %v", err)
+				}
+				_, err = session.Conn.Write(bytes)
+				if err != nil {
+					log.Fatalf("failed to write: %v", err)
+				}
+				session.State = OpenSent
+			case TcpConnectionFails:
+				session.State = Active
+			default:
+				log.Fatalf("unknown event: %v", event)
+			}
+		case OpenSent:
+			msg := open_MSG{}
+			err := binary.Read(session.Conn, binary.BigEndian, &msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				return
+			}
+			log.Printf("msg: %v", msg)
+			session.State = OpenConfirm
+
+		case OpenConfirm:
+		case Established:
 		}
-		log.Printf("msg: %v", msg)
+
 	}
 }
 func passive_mode() {
-	l, err := net.Listen("tcp", ":179")
+
+	ifis, err := net.Interfaces()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to get interfaces: %v", err)
 	}
-	defer l.Close()
-	for {
-		conn, err := l.Accept()
-		if err != nil {
+	for _, ifi := range ifis {
+		if is_loopback(ifi) {
 			continue
 		}
-		go handle_connection(conn)
+		go handle_bgp_passive(ifi)
 	}
+	select {}
 }
